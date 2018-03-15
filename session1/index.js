c795880bfc569856c1ce5f5b4185692f1060f0e2
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-03-15 11:18:08 +0000

    Sessions 1 & 2
    Originals from Traversy Media's JavaScript Cardio

---
+++ session1/index.js
@@ -0,0 +1,92 @@
+// CHALLENGE 1: REVERSE A STRING
+// Return a string in reverse
+// ex. reverseString('hello') === 'olleh'
+const reverseString= {
+
+}
+
+
+// CHALLENGE 2: VALIDATE A PALINDROME
+// Return true if palindrome and false if not
+// ex. isPalindrome('racecar') === 'true', isPalindrome('hello') == false
+
+const isPalindrome = {
+
+}
+
+// CHALLENGE 3: REVERSE AN INTEGER
+// Return an integer in reverse
+// ex. reverseInt(521) === 125
+
+const reverseInt = {
+
+}
+
+
+// CHALLENGE 4: CAPITALISE LETTERS
+// Return a string with the first letter of every word capitalised
+// ex. capitaliseLetters('i love javascript') === 'I Love Javascript'
+const capitaliseLetters = {
+
+}
+
+
+// CHALLENGE 5: MAX CHARACTER
+// Return the character that is most common in a string
+// ex. maxCharacter('javascript') == 'a'
+// If the answer is multiple characters then return as an array
+const maxCharacter = {
+
+}
+
+
+
+// CHALLENGE 6: FIZZBUZZ
+// Write a program that prints all the numbers, seperated by spaces,
+// from start to finish, where start defaults to 1 and end to 100.
+// For multiples of 3, instead of the number, print "Fizz",
+// for multiples of 5 print "Buzz". For numbers which are multiples of both 3 and 5,
+// print "FizzBuzz".
+const fizzBuzz = {
+
+}
+
+// Test data
+
+const testWords = [
+    '',
+    'a',
+    'hello',
+    'racecar',
+    'potato',
+    'javascript',
+    123,
+];
+
+const testStrings = [
+    '',
+    'hEllo, THIS is A tesT',
+    '1 a string with numbers 2',
+    'one',
+    123,
+]
+
+const testInt = [
+    0,
+    42,
+    12345678,
+    10,
+    101,
+    -5,
+    -987,
+    'a',
+];
+
+export default {
+    reverseString,
+    isPalindrome,
+    reverseInt,
+    capitaliseLetters,
+    maxCharacter,
+    fizzBuzz,
+};
---
+++ session2/index.js
@@ -0,0 +1,48 @@
+// CHALLENGE 1: LONGEST WORD
+// Return the longest word of a string
+// ex. longestWord('Hi there, my name is Brad') === 'there,'
+// if multiple words share the same length return as an array instead
+const longestWord = {
+
+}
+
+// CHALLENGE 2: ARRAY CHUNKING
+// Split an array into chunked arrays of a specific length
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 3) === [[1, 2, 3],[4, 5, 6],[7]]
+// ex. chunkArray([1, 2, 3, 4, 5, 6, 7], 2) === [[1, 2],[3, 4],[5, 6],[7]]
+const chunkArray = {
+
+}
+
+// CHALLENGE 3: FLATTEN ARRAY
+// Take an array of arrays and flatten to a single array
+// ex. [[1, 2], [3, 4], [5, 6], [7]] = [1, 2, 3, 4, 5, 6, 7]
+const flattenArray = {
+
+}
+
+// CHALLENGE 4: ANAGRAM
+// Return true if anagram and false if not
+// case insensitive
+// ex. 'elbow' === 'below'
+// ex. 'Dormitory' === 'dirty room##'
+const isAnagram = {
+
+}
+
+// CHALLENGE 5: LETTER CHANGES
+// Change every letter of the string to the one that follows it
+// Capitalize the vowels and lowercase all other letters
+// Z should turn to A
+// ex. 'hello there' === 'Ifmmp UIfsf'
+const letterChanges = {
+
+}
+
+export default {
+    longestWord,
+    chunkArray,
+    flattenArray,
+    isAnagram,
+    letterChanges,
+}
