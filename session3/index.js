ad058f017087bbc71b5ba2a5c96efde51a2d6464
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-03-15 11:18:57 +0000

    Session 3
    Original questions from HackerRank

---
+++ session3/index.js
@@ -0,0 +1,27 @@
+// CHALLENGE 1: TIME CONVERSION
+// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+//
+// Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock.
+// Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+//
+// Example: timeConvert('07:05:45PM') -> '19:05:45'
+const timeConvert = {
+
+}
+
+// CHALLENGE 2: MINIMUM - MAXIMUM SUM
+// Given five positive integers, find the minimum and maximum values that can
+// be calculated by summing exactly four of the five integers.
+// Then print the respective minimum and maximum values as a single line
+// of two space-separated long integers.
+//
+// Example: minMaxSum('1 2 3 4 5') -> '10 14'
+const minMaxSum = {
+
+}
+
+
+export default {
+    timeConvert,
+    minMaxSum,
+}
