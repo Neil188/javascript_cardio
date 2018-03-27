99980a93364502394f5b030631f3bebe3df497bb
Neil188 <19958604+Neil188@users.noreply.github.com>
2018-03-27 21:27:52 +0100

    Add time difference challenge

--- session3/index.js
+++ session3/index.js
@@ -1,5 +1,6 @@
 // CHALLENGE 1: TIME CONVERSION
-// Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
+// Given a time in 12-hour AM/PM format ('HH:MM:SSAA' HH hours, MM Mins, SS Seconds,
+// AA AM or PM), convert it to military (24-hour) time ('HH:MM:SS').
 //
 // Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock.
 // Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
@@ -8,8 +9,21 @@
 const timeConvert = {
 
 }
+// CHALLENGE 2: TIME DIFFERENCE
+// Given 2 times in 12-hour AM/PM format ('HH:MM:SSAA' HH hours, MM Mins, SS Seconds,
+// AA AM or PM), where the second time is always ahead of the first (so if
+// hour < first time, this is the next day), find the difference between the
+// two times, returning answer in format 'HH:MM:SS'
+//
+// Note: Midnight is 12:00:00AM on a 12-hour clock, and 00:00:00 on a 24-hour clock.
+// Noon is 12:00:00PM on a 12-hour clock, and 12:00:00 on a 24-hour clock.
+//
+// Example: timeConvert('07:05:45PM', '09:12:50PM') -> '02:07:05'
+const timeDiff = {
 
-// CHALLENGE 2: MINIMUM - MAXIMUM SUM
+}
+
+// CHALLENGE 3: MINIMUM - MAXIMUM SUM
 // Given five positive integers, find the minimum and maximum values that can
 // be calculated by summing exactly four of the five integers.
 // Then print the respective minimum and maximum values as a single line
@@ -23,5 +37,6 @@ const minMaxSum = {
 
 export default {
     timeConvert,
+    timeDiff,
     minMaxSum,
 }
